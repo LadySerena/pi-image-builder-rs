56618bd2c70600143f285823ec7b3086775c0176
LadySerena <serena.tiede+github@gmail.com>
2022-11-16 22:35:19 -0600

    refactor: start rewrite for media download
    I'm moving to archlinux arm and I need to support md5 verification
    (should also look at signature verification since md5 is yikes)

--- src/fetch_media/mod.rs
+++ src/fetch_media/mod.rs
@@ -6,20 +6,62 @@ use std::time::Duration;
 use reqwest::blocking::{Client, ClientBuilder};
 use reqwest::Url;
 
-pub fn download_if_needed(force_overwrite: bool, urls: Vec<&str>) {
+// pub struct Download(Url, Url);
+//
+// impl Download {
+//     pub fn new(media: &str, hash: &str) -> Download {
+//         let media_parsed = Url::parse(media).unwrap();
+//         let hash_parsed = Url::parse(hash).unwrap();
+//         Download {
+//             0: media_parsed,
+//             1: hash_parsed,
+//         }
+//     }
+// //todo convert to return a slice
+//     pub fn get_filenames(&self) -> Sli<String> {
+//         (
+//             self.0.path_segments().unwrap().last().unwrap().to_string(),
+//             self.1.path_segments().unwrap().last().unwrap().to_string(),
+//         )
+//     }
+// }
+
+pub fn download_if_needed(force_overwrite: bool, urls: &[&str]) {
+    let client = ClientBuilder::new()
+        .timeout(Duration::from_secs(600))
+        .build()
+        .unwrap();
+    // the intent is to get the file names for the hash verification
+    let files: Vec<String> = urls
+        .iter()
+        .map(|url_string| {
+            let parsed_url = Url::parse(url_string).unwrap();
+            let file_name = parsed_url.path_segments().unwrap().last().unwrap();
+            let output = Path::new(file_name);
+            if force_overwrite || !check_if_file_exists(output) {
+                download_file(parsed_url.clone(), output, client.clone());
+            }
+            file_name.to_string()
+        })
+        .collect();
+}
+
+pub fn download_if_needed2(force_overwrite: bool, download: Download) {
     let client = ClientBuilder::new()
         .timeout(Duration::from_secs(600))
         .build()
         .unwrap();
 
-    for url in urls {
-        let parsed_url = Url::parse(url).unwrap();
-        let file_name = parsed_url.path_segments().unwrap().last().unwrap();
-        let output = Path::new(file_name);
-        if force_overwrite || !check_if_file_exists(output) {
-            download_file(parsed_url.clone(), output, client.clone());
-        }
-    }
+    let files = download.get_filenames();
+
+    // for url in urls {
+    //     let parsed_url = Url::parse(url).unwrap();
+    //     let file_name = parsed_url.path_segments().unwrap().last().unwrap();
+    //     let output = Path::new(file_name);
+    //     if force_overwrite || !check_if_file_exists(output) {
+    //         download_file(parsed_url.clone(), output, client.clone());
+    //     }
+    // }
 }
 
 fn download_file(url: Url, file_name: &Path, client: Client) {
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ extern crate core;
 #[macro_use(defer)]
 extern crate scopeguard;
 
-use size::Size;
 use std::borrow::Borrow;
+
+use size::Size;
+
+use crate::fetch_media::{download_if_needed, download_if_needed2};
 
 mod compression;
 mod configuration;
@@ -38,6 +41,17 @@ fn main() {
     defer!(image.detach());
     println!("{}", image.device.path().unwrap().to_str().unwrap());
     filesystem::create(image.borrow());
+    let base_url = "http://os.archlinuxarm.org/os/";
+    let image_file = "ArchLinuxARM-rpi-aarch64-latest.tar.gz";
+    let hash_file = "ArchLinuxARM-rpi-aarch64-latest.tar.gz.md5";
+    download_if_needed(
+        false,
+        [
+            get_urls(base_url, image_file).as_str(),
+            get_urls(base_url, hash_file).as_str(),
+        ]
+        .borrow(),
+    );
 }
 
 fn get_urls(base: &str, file: &str) -> String {
